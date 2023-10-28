894dde13788eae51bfb9292c78b8c1da7c0b66b7
Goran Horvatic <goranhorvatic02@gmail.com>
2023-10-29 00:54:17 +0200

    small changes in feed

--- src/Feed.js
+++ src/Feed.js
@@ -1,14 +1,19 @@
 import React from 'react'
+import "./Feed.css";
+import { Button } from "@mui/material";
 
 function Feed() {
   return (
-    <div>
+    <div className='feed'>
         {/* Header */}
 
         <h2>Home</h2>
 
         {/* TweetBox */}
-
+        <textarea />
+        <Button className='tweetButton' variant="outlined">
+          Tweet
+        </Button>
         {/* Post */}
     </div>
   )
